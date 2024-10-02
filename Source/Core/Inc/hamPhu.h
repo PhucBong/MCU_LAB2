0b5462ff6994c4ffff8f9060e9cbc142608b05f4
PhucBong <truongdinhphuc1523@gmail.com>
2024-10-03 00:28:22 +0700

    finish ex9

--- Source/Core/Inc/hamPhu.h
+++ Source/Core/Inc/hamPhu.h
@@ -10,5 +10,8 @@
 #include "main.h"
 
 void display7SEG(int num);
+void initRow();
+void updateLEDMatrix(int index);
+void displayLEDMatrix(uint8_t value);
 
 #endif /* INC_HAMPHU_H_ */
--- Source/Core/Src/hamPhu.c
+++ Source/Core/Src/hamPhu.c
@@ -103,3 +103,66 @@ void display7SEG(int num){
 			break;
 	}
 }
+
+uint16_t ENM_Pin[8] = {ENM0_Pin, ENM1_Pin, ENM2_Pin, ENM3_Pin, ENM4_Pin, ENM5_Pin, ENM6_Pin, ENM7_Pin};
+void initRow(){
+	HAL_GPIO_WritePin(GPIOB, ROW0_Pin | ROW1_Pin | ROW2_Pin | ROW3_Pin | ROW4_Pin | ROW5_Pin | ROW6_Pin | ROW7_Pin, SET);
+}
+void displayLEDMatrix(uint8_t value){
+	  for (int col = 0; col < 8; col++){
+		  int flag;
+		  if(value & (1 << (7 - col))) flag = 1;
+		  if(!(value & (1 << (7 - col)))) flag = 0;
+		  switch (flag) {
+			case 0:
+				HAL_GPIO_WritePin(GPIOA, ENM_Pin[col], SET);
+				break;
+			case 1:
+				HAL_GPIO_WritePin(GPIOA, ENM_Pin[col], RESET);
+				break;
+			default:
+				break;
+		}
+	  }
+  }
+uint8_t matrix_buffer[8] = {0x3c,0x7e,0xc3,0xff,0xff,0xc3,0xc3,0xc3}; // gan cho cot
+
+void updateLEDMatrix(int index){
+	  initRow();
+	  switch (index) {
+		case 0:
+			HAL_GPIO_WritePin(ROW0_GPIO_Port, ROW0_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		case 1:
+			HAL_GPIO_WritePin(ROW1_GPIO_Port, ROW1_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		case 2:
+			HAL_GPIO_WritePin(ROW2_GPIO_Port, ROW2_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		case 3:
+			HAL_GPIO_WritePin(ROW3_GPIO_Port, ROW3_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		case 4:
+			HAL_GPIO_WritePin(ROW4_GPIO_Port, ROW4_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		case 5:
+			HAL_GPIO_WritePin(ROW5_GPIO_Port, ROW5_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		case 6:
+			HAL_GPIO_WritePin(ROW6_GPIO_Port, ROW6_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		case 7:
+			HAL_GPIO_WritePin(ROW7_GPIO_Port, ROW7_Pin, RESET);
+			displayLEDMatrix(matrix_buffer[index]);
+			break;
+		default:
+			break;
+	}
+  }
